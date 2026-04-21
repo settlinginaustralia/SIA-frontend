c7b95f2f62f88074482ed88d1ea0286d58a4d43b
settlinginaustralia <settlinginaustralia@gmail.com>
2026-04-21 23:49:41 +0000

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { useLanguage } from '../context/LanguageContext'
 import { pathSelectionLinks } from '../data/pathSelectionLinks'
 import { studyPathSlides } from '../data/studyPathSlides'
@@ -26,22 +25,18 @@ function Home() {
             <ul className="sia-hero__brandTrack">
               {pathSelectionLinks.map((item) => (
                 <li key={item.href}>
-                  <Link className="sia-hero__brand" to={item.href}>
+                  <span className="sia-hero__brand">
                     <i className={`sia-hero__brandIcon ${item.icon}`} aria-hidden="true" />
                     <span className="sia-hero__brandLabel">{t(item.labelKey)}</span>
-                  </Link>
+                  </span>
                 </li>
               ))}
               {pathSelectionLinks.map((item) => (
                 <li key={`${item.href}--marquee`} aria-hidden="true">
-                  <Link
-                    className="sia-hero__brand sia-hero__brand--marqueeDup"
-                    to={item.href}
-                    tabIndex={-1}
-                  >
+                  <span className="sia-hero__brand sia-hero__brand--marqueeDup">
                     <i className={`sia-hero__brandIcon ${item.icon}`} aria-hidden="true" />
                     <span className="sia-hero__brandLabel">{t(item.labelKey)}</span>
-                  </Link>
+                  </span>
                 </li>
               ))}
             </ul>
